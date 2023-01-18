981fe29f655fb6253c108b840bd2119a56faef58
Amman Vedi <av@Ammans-MacBook-Pro.local>
2023-01-18 00:26:14 +0000

    update rate

--- infra/lambda/main.tf
+++ infra/lambda/main.tf
@@ -82,3 +82,26 @@ resource "aws_lambda_function" "terraform_lambda_func" {
     }
   }
 }
+
+resource "aws_cloudwatch_event_rule" "schedule" {
+  name = "schedule"
+  description = "Schedule for Lambda Function"
+  schedule_expression = "rate(3 hours)"
+}
+
+resource "aws_cloudwatch_event_target" "schedule_lambda" {
+  rule = aws_cloudwatch_event_rule.schedule.name
+  target_id = "processing_lambda"
+  arn = aws_lambda_function.terraform_lambda_func.arn
+  depends_on = [aws_lambda_function.terraform_lambda_func, aws_cloudwatch_event_rule.schedule]
+}
+
+
+resource "aws_lambda_permission" "allow_events_bridge_to_run_lambda" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_func.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.schedule.arn
+  depends_on = [aws_lambda_function.terraform_lambda_func, aws_cloudwatch_event_rule.schedule]
+}
